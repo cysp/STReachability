eed853ebf8b8a46f2aa39a5ffe13eb200d4c3af6
Scott Talbot <s@chikachow.org>
2012-09-26 19:54:40 +1000

    License under the MPL 2.0

--- STReachability/STReachability.h
+++ STReachability/STReachability.h
@@ -2,8 +2,11 @@
 //  STReachability.h
 //  STReachability
 //
-//  Created by Scott Talbot on 11/08/12.
-//  Copyright (c) 2012 Scott Talbot. All rights reserved.
+//  This Source Code Form is subject to the terms of the Mozilla Public
+//  License, v. 2.0. If a copy of the MPL was not distributed with this
+//  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+//  Copyright (c) 2012 Scott Talbot.
 //
 
 #import <Foundation/Foundation.h>
--- STReachability/STReachability.m
+++ STReachability/STReachability.m
@@ -2,8 +2,11 @@
 //  STReachability.m
 //  STReachability
 //
-//  Created by Scott Talbot on 11/08/12.
-//  Copyright (c) 2012 Scott Talbot. All rights reserved.
+//  This Source Code Form is subject to the terms of the Mozilla Public
+//  License, v. 2.0. If a copy of the MPL was not distributed with this
+//  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+//  Copyright (c) 2012 Scott Talbot.
 //
 
 #if ! (defined(__has_feature) && __has_feature(objc_arc))
