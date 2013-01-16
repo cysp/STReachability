39871afc0a423baad8611f8d022b3a946a9386b8
Scott Talbot <s@chikachow.org>
2013-01-16 23:54:44 +1100

    Add unit test skeleton

---
+++ STReachabilityTests/STReachabilityTests.h
@@ -0,0 +1,13 @@
+//
+//  STReachabilityTests.h
+//  STReachability
+//
+//  Copyright (c) 2013 Scott Talbot. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface STReachabilityTests : SenTestCase
+
+@end
---
+++ STReachabilityTests/STReachabilityTests.m
@@ -0,0 +1,23 @@
+//
+//  STReachabilityTests.m
+//  STReachability
+//
+//  Copyright (c) 2013 Scott Talbot. All rights reserved.
+//
+
+#import "STReachabilityTests.h"
+
+#import "STReachability.h"
+
+
+@implementation STReachabilityTests
+
+- (void)setUp {
+	[super setUp];
+}
+
+- (void)tearDown {
+	[super tearDown];
+}
+
+@end
