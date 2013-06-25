bafcbede950b5b6d11f035f0a0054c5ba50b138f
Scott Talbot <s@chikachow.org>
2013-06-26 08:35:52 +1000

    Add some documentation

--- STReachability/STReachability+Mocking.h
+++ STReachability/STReachability+Mocking.h
@@ -10,7 +10,18 @@
 
 @interface STReachability (Mocking)
 
+/**
+ * Enable or disable mocking of STReachability objects created after this call
+ *
+ * @param mocking whether or not to create mocked objects
+ */
 + (void)setMocking:(BOOL)mocking;
+
+/**
+ * Trigger reachability changes in mocked objects
+ *
+ * @param status the new reachability status for mocked objects
+ */
 + (void)setMockReachabilityStatus:(enum STReachabilityStatus)status;
 
 @end
--- STReachability/STReachability.h
+++ STReachability/STReachability.h
@@ -19,21 +19,59 @@ NS_ENUM(NSInteger, STReachabilityStatus) {
 	STReachabilityStatusReachableViaWWAN = 2,
 };
 
+/**
+ * Determine whether a status code implies reachability
+ */
 extern BOOL STReachabilityStatusIsReachable(enum STReachabilityStatus);
+/**
+ * Determine whether a status code implies unreachability
+ */
 extern BOOL STReachabilityStatusIsUnreachable(enum STReachabilityStatus);
 
 
 typedef void(^STReachabilityBlock)(enum STReachabilityStatus status, enum STReachabilityStatus previousStatus);
 
 
+/**
+ * Provides an Objective-C interface onto SCNetworkReachability
+ */
 @interface STReachability : NSObject
 
+/** @name Creation Methods */
+
+/**
+ * Create a STReachability object for "The Internet"
+ *
+ * @see +reachabilityWithBlock:
+ */
 + (STReachability *)reachability;
+/**
+ * Create a STReachability object for "The Internet"
+ *
+ * @param block a handler block to execute when connectivity changes
+ */
 + (STReachability *)reachabilityWithBlock:(STReachabilityBlock)block;
 
+/**
+ * Create a STReachability object for a specific host
+ *
+ * @param hostname the host to use for reachability (hostname or literal address)
+ * @see +reachabilityWithHost:block:
+ */
 + (STReachability *)reachabilityWithHost:(NSString *)hostname;
+/**
+ * Create a STReachability object for a specific host
+ *
+ * @param hostname the host to use for reachability (hostname or literal address)
+ * @param block a handler block to execute when connectivity changes
+ */
 + (STReachability *)reachabilityWithHost:(NSString *)hostname block:(STReachabilityBlock)block;
 
+/** @name Status Methods */
+
+/**
+ * The latest reachability status seen
+ */
 @property (nonatomic,assign,readonly) enum STReachabilityStatus status;
 
 @end
