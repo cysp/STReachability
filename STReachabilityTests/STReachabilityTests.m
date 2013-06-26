385c21d50b730f716f2741aa6d81044fdb3ef412
Scott Talbot <s@chikachow.org>
2013-06-26 18:34:02 +1000

    Sanity-check instantiation in tests

--- STReachabilityTests/STReachabilityTests.m
+++ STReachabilityTests/STReachabilityTests.m
@@ -33,51 +33,81 @@
 	{
 		__block NSUInteger nReachabilityStatusChangesSeen = 0;
 
-		[STReachability reachabilityWithBlock:^(enum STReachabilityStatus status, enum STReachabilityStatus previousStatus) {
+		STReachability * const reachability = [STReachability reachabilityWithBlock:^(enum STReachabilityStatus status, enum STReachabilityStatus previousStatus) {
 			++nReachabilityStatusChangesSeen;
 		}];
+		STAssertNotNil(reachability, @"");
+		if (!reachability) {
+			return;
+		}
 
 		STAssertEquals(nReachabilityStatusChangesSeen, (NSUInteger)0, @"", nil);
+
+		(void)reachability;
 	}
 
 	{
 		__block NSUInteger nReachabilityStatusChangesSeen = 0;
 
-		[STReachability reachabilityWithBlock:^(enum STReachabilityStatus status, enum STReachabilityStatus previousStatus) {
+		STReachability * const reachability = [STReachability reachabilityWithBlock:^(enum STReachabilityStatus status, enum STReachabilityStatus previousStatus) {
 			++nReachabilityStatusChangesSeen;
 		}];
+		STAssertNotNil(reachability, @"");
+		if (!reachability) {
+			return;
+		}
 
 		[STReachability setMockReachabilityStatus:STReachabilityStatusReachableViaWifi];
 
 		STAssertEquals(nReachabilityStatusChangesSeen, (NSUInteger)1, @"", nil);
+
+		(void)reachability;
 	}
 
 	{
 		__block NSUInteger nReachabilityStatusChangesSeen = 0;
 
-		[STReachability reachabilityWithBlock:^(enum STReachabilityStatus status, enum STReachabilityStatus previousStatus) {
+		STReachability * const reachability = [STReachability reachabilityWithBlock:^(enum STReachabilityStatus status, enum STReachabilityStatus previousStatus) {
 			++nReachabilityStatusChangesSeen;
 		}];
+		STAssertNotNil(reachability, @"");
+		if (!reachability) {
+			return;
+		}
 
 		[STReachability setMockReachabilityStatus:STReachabilityStatusReachableViaWifi];
 		[STReachability setMockReachabilityStatus:STReachabilityStatusReachableViaWWAN];
 
 		STAssertEquals(nReachabilityStatusChangesSeen, (NSUInteger)2, @"", nil);
+
+		(void)reachability;
 	}
 
 	{
 		__block NSUInteger nReachabilityStatusChangesSeen = 0;
 
-		[STReachability reachabilityWithBlock:^(enum STReachabilityStatus status, enum STReachabilityStatus previousStatus) {
+		STReachability * const reachability1 = [STReachability reachabilityWithBlock:^(enum STReachabilityStatus status, enum STReachabilityStatus previousStatus) {
 			++nReachabilityStatusChangesSeen;
 		}];
-		[STReachability reachabilityWithBlock:^(enum STReachabilityStatus status, enum STReachabilityStatus previousStatus) {
+		STAssertNotNil(reachability1, @"");
+		if (!reachability1) {
+			return;
+		}
+
+		STReachability * const reachability2 = [STReachability reachabilityWithBlock:^(enum STReachabilityStatus status, enum STReachabilityStatus previousStatus) {
 			++nReachabilityStatusChangesSeen;
 		}];
+		STAssertNotNil(reachability2, @"");
+		if (!reachability2) {
+			return;
+		}
 
 		[STReachability setMockReachabilityStatus:STReachabilityStatusReachableViaWifi];
 
 		STAssertEquals(nReachabilityStatusChangesSeen, (NSUInteger)2, @"", nil);
+
+		(void)reachability1;
+		(void)reachability2;
 	}
 }
 
@@ -99,9 +129,13 @@
 
 - (void)testReachabilityInternet {
 	NSMutableArray * const reachabilityStatusesSeen = [[NSMutableArray alloc] initWithCapacity:1];
-	[STReachability reachabilityWithBlock:^(enum STReachabilityStatus status, enum STReachabilityStatus previousStatus) {
+	STReachability * const reachability = [STReachability reachabilityWithBlock:^(enum STReachabilityStatus status, enum STReachabilityStatus previousStatus) {
 		[reachabilityStatusesSeen addObject:@(status)];
 	}];
+	STAssertNotNil(reachability, @"");
+	if (!reachability) {
+		return;
+	}
 
 	NSDate * const resolutionTimeout = [NSDate dateWithTimeIntervalSinceNow:.5];
 	[[NSRunLoop mainRunLoop] runUntilDate:resolutionTimeout];
@@ -111,13 +145,19 @@
 	if (reachabilityStatusesSeenCount > 0) {
 		STAssertTrue(STReachabilityStatusIsReachable([reachabilityStatusesSeen[0] unsignedIntegerValue]), @"", nil);
 	}
+
+	(void)reachability;
 }
 
 - (void)testReachabilityLiteralAddressIPv4 {
 	NSMutableArray * const reachabilityStatusesSeen = [[NSMutableArray alloc] initWithCapacity:1];
-	[STReachability reachabilityWithHost:@"8.8.8.8" block:^(enum STReachabilityStatus status, enum STReachabilityStatus previousStatus) {
+	STReachability * const reachability = [STReachability reachabilityWithHost:@"8.8.8.8" block:^(enum STReachabilityStatus status, enum STReachabilityStatus previousStatus) {
 		[reachabilityStatusesSeen addObject:@(status)];
 	}];
+	STAssertNotNil(reachability, @"");
+	if (!reachability) {
+		return;
+	}
 
 	NSDate * const resolutionTimeout = [NSDate dateWithTimeIntervalSinceNow:.5];
 	[[NSRunLoop mainRunLoop] runUntilDate:resolutionTimeout];
@@ -127,15 +167,21 @@
 	if (reachabilityStatusesSeenCount > 0) {
 		STAssertTrue(STReachabilityStatusIsReachable([reachabilityStatusesSeen[0] unsignedIntegerValue]), @"", nil);
 	}
+
+	(void)reachability;
 }
 
 - (void)testReachabilityNameResolutionFailure {
 	NSMutableArray * const reachabilityStatusesSeen = [[NSMutableArray alloc] initWithCapacity:1];
-	[STReachability reachabilityWithHost:@"nonexistent.example.org" block:^(enum STReachabilityStatus status, enum STReachabilityStatus previousStatus) {
+	STReachability * const reachability = [STReachability reachabilityWithHost:@"nonexistent.example.org" block:^(enum STReachabilityStatus status, enum STReachabilityStatus previousStatus) {
 		[reachabilityStatusesSeen addObject:@(status)];
 	}];
+	STAssertNotNil(reachability, @"");
+	if (!reachability) {
+		return;
+	}
 
-	NSDate * const resolutionTimeout = [NSDate dateWithTimeIntervalSinceNow:.5];
+	NSDate * const resolutionTimeout = [NSDate dateWithTimeIntervalSinceNow:1];
 	[[NSRunLoop mainRunLoop] runUntilDate:resolutionTimeout];
 
 	NSUInteger const reachabilityStatusesSeenCount = [reachabilityStatusesSeen count];
@@ -143,13 +189,19 @@
 	if (reachabilityStatusesSeenCount > 0) {
 		STAssertTrue(STReachabilityStatusIsUnreachable([reachabilityStatusesSeen[0] unsignedIntegerValue]), @"", nil);
 	}
+
+	(void)reachability;
 }
 
 - (void)testReachabilityNameResolutionSuccess {
 	NSMutableArray * const reachabilityStatusesSeen = [[NSMutableArray alloc] initWithCapacity:1];
-	[STReachability reachabilityWithHost:@"example.org" block:^(enum STReachabilityStatus status, enum STReachabilityStatus previousStatus) {
+	STReachability * const reachability = [STReachability reachabilityWithHost:@"example.org" block:^(enum STReachabilityStatus status, enum STReachabilityStatus previousStatus) {
 		[reachabilityStatusesSeen addObject:@(status)];
 	}];
+	STAssertNotNil(reachability, @"");
+	if (!reachability) {
+		return;
+	}
 
 	NSDate * const resolutionTimeout = [NSDate dateWithTimeIntervalSinceNow:.5];
 	[[NSRunLoop mainRunLoop] runUntilDate:resolutionTimeout];
@@ -159,6 +211,8 @@
 	if (reachabilityStatusesSeenCount > 0) {
 		STAssertTrue(STReachabilityStatusIsReachable([reachabilityStatusesSeen[0] unsignedIntegerValue]), @"", nil);
 	}
+
+	(void)reachability;
 }
 
 @end
